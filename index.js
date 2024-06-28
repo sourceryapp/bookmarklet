ffb1cdf7a0f867ae96d4b6a3c3b52644108c8f8b
Brian Daley <brian@uconn.edu>
2024-06-28 16:44:56 +0000

    close to dunzo

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ app.use(express.static('public'))
 app.use(bodyParser.text());
 
 
+/**
+ * This function mimics the functionality of the production
+ * function that (at the time of writing) will be deployed to Supabase Edge.
+ * 
+ * This utilizes the Zotero translation server to translate the URL
+ * into structured data, which is passed to the Sourcery request form.
+ */
 app.get('/api/fake-supabase-edge-function', async (req, res) => {
     let { url } = req.query;
     let response, data
+    let redirect_url = new URL(config.sourcery_request_url);
+
 
     console.log('Request received:', url)
 
@@ -28,16 +37,35 @@ app.get('/api/fake-supabase-edge-function', async (req, res) => {
             body: url
         });
         data = await response.json();
-        res.send(data)
+
+        // Extract the title from the data
+        redirect_url.searchParams.append("title", data[0].title);
+
+        // @todo format the data to be more readable (rather than JSON stringified)
+        redirect_url.searchParams.append("message", JSON.stringify(data[0], null, 4));
+
+        // redirect to redirect_url
+        res.redirect(redirect_url.toString());
     } catch (error) {
         console.error('Error:', error)
         res.send(error)
     }
 }) 
 
-app.post('/api/minify', async (req, res) => {
-    console.log('Request recieved:', jsmin(req.body))
-    res.send(jsmin(req.body))
+/**
+ * Just a simple endpoint to minify JS code.
+ * Should probably switch to something better than jsmin.
+ * (maybe terser?)
+ */
+app.post('/api/minify', (req, res) => {
+    let minifiedCode
+    try {
+        minifiedCode = jsmin(req.body)
+        console.log('Minified code:', minifiedCode)
+        res.send(minifiedCode)
+    } catch (error) {
+        res.send(error)
+    }
 }) 
 
 
