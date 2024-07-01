ce305518b8a141bb6d7b57d3afbdd874889b1ac0
Brian Daley <brian@uconn.edu>
2024-07-01 17:01:32 +0000

    updated endpoint

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 export default {
     dev_endpoint: 'http://127.0.0.1:3000/api/fake-supabase-edge-function',
-    prod_endpoint: 'https://csfzqcahdlzghhzgbvtq.supabase.co/functions/v1/request', // TBD
+    prod_endpoint: 'https://csfzqcahdlzghhzgbvtq.supabase.co/functions/v1/translate',
     translation_server: 'https://ts.sourceryapp.org/web',
     sourcery_request_url: 'https://sourceryapp.org/request/create'
 }
